e2ab6a4f55ef8f841fb1031be999a295aadc9bd6
BlessingOkora <moraablessing082@gmail.com>
2024-12-14 17:41:14 +0300

    creates initializers and properties

--- models/article.py
+++ models/article.py
@@ -1,10 +1,56 @@
+from database.connection import get_db_connection
+from models.author import Author
+from models.magazine import Magazine
+
 class Article:
-    def __init__(self, id, title, content, author_id, magazine_id):
-        self.id = id
-        self.title = title
-        self.content = content
-        self.author_id = author_id
-        self.magazine_id = magazine_id
+    def __init__(self, title, author, magazine):
+        if not isinstance(author, Author):
+            raise ValueError("Author must be an instance of the Author class.")
+        if not isinstance (magazine, Magazine):
+            raise ValueError("Magazine must be an instance of the Magazine class.")
+        if not isinstance(title, str) or not (5 <= len(title.strip()) <= 50):
+            raise ValueError("title must be a string between 5 and 50 characters.")
+        
+        self._author = author
+        self._magazine = magazine
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        if not hasattr(self, '_id'):
+            cursor.execute(
+                "INSERT INTO articles (title, author_id, magazine_id) VALUES (?, ?, ?)",
+                (title, author.id, magazine.id),
+            )
+            self._id = cursor.lastrowid
+            self._title = title
+            conn.commit()
+        else:
+            cursor.execute(
+                "SELECT title FROM articles WHERE id = ?", (self._id,)
+            )
+            row = cursor.fetchone()
+            if row:
+                self._title = row["title"]
+            else:
+                raise ValueError(f"No article found with id {self._id}.")
+            conn.close()
+        
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @property
+    def author(self):
+        return self._author
+    
+    @property
+    def magazine(self):
+        return self._magazine
 
     def __repr__(self):
         return f'<Article {self.title}>'
--- models/magazine.py
+++ models/magazine.py
@@ -1,8 +1,74 @@
+from database.connection import get_db_connection
+
 class Magazine:
-    def __init__(self, id, name, category):
-        self.id = id
-        self.name = name
-        self.category = category
+    def __init__(self, id = None, name = None, category = None):
+        if id is not None and not isinstance(id, int):
+            raise ValueError("id must be an integer.")
+        if name is not None and (not isinstance(name,str) or not (2 <= len(name.strip()) <= 16)):
+            raise ValueError("name must be a string between 2 and 16 characters.")
+        if category is not None and (not isinstance(category, str) or len(category.strip()) == 0):
+            raise ValueError("category must be a non empty string.")
+        
+        if id is None:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            cursor.execute(
+                "INSERT INTO magazines (nae, category) VALUES (?, ?), (name, category)"
+            )
+            self._id = cursor.lastrowid
+            self._name = name
+            self._category = category
+            conn.commit()
+            conn.close()
+        else:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            cursor.execute("SELECT name, category FROM magazines WHERE id = ?", (id,))
+            row = cursor.fetchone()
+            conn.close()
+
+            if row:
+                self._id = id
+                self._name = row["name"]
+                self._category = row["category"]
+            else:
+                raise ValueError(f"No magazine found with ID {id}.")
+            
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (2 <= len(value.strip()) <= 16):
+            raise ValueError("name must be a string between 2 and 16 characters.")
+        
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE magazines SET name = ? WHERE id = ?", (value, self._id))
+        conn.commit()
+        conn.close()
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str) or len (value.strip()) == 0:
+            raise ValueError("category must be a non empty string.")
+        
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE magazines SET category = ? WHERE id = ?", (value, self._id))
+        conn.commit()
+        conn.close()
+        self._category = value
 
     def __repr__(self):
         return f'<Magazine {self.name}>'
